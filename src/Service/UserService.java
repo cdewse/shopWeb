f11ef53e70f715d98b1f0c6df76e16af9dfef24b
cdewse <xswqaw@gmail.com>
2017-05-16 21:33:34 +0800

    login add a param(String password）

--- src/Service/UserService.java
+++ src/Service/UserService.java
@@ -19,7 +19,11 @@ public class UserService {
 
     }
 
-    public String Login(String username){
-        return userDao.findPassword(username);
+    //用户名密码参数跟数据匹配，相同返回true否则false
+    public boolean Login(String username,String password){
+        if(userDao.findPassword(username).equals(password)){
+            return true;
+        }
+        return false;
     }
 }

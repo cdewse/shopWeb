ca001c84d1f8fddc26483dc5132cec4c0f77394f
cdewse <xswqaw@gmail.com>
2017-05-16 21:56:59 +0800

    none

--- src/Service/UserService.java
+++ src/Service/UserService.java
@@ -2,6 +2,7 @@ package Service;
 
 import Dao.UserDao;
 import Entity.User;
+import utils.Encryption;
 
 /**
  * Created by cdewse on 17-5-15.
@@ -21,7 +22,7 @@ public class UserService {
 
     //用户名密码参数跟数据匹配，相同返回true否则false
     public boolean Login(String username,String password){
-        if(userDao.findPassword(username).equals(password)){
+        if(userDao.findPassword(username).equals(Encryption.encrypt(password))){
             return true;
         }
         return false;
